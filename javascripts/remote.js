9b6e42bf4eee0149ef9708fe4940ffcded34df9b
Noah <njhamann@gmail.com>
2013-12-27 22:02:47 -0500

    changes

--- javascripts/remote.js
+++ javascripts/remote.js
@@ -8,6 +8,7 @@ stApp.remote.run(['$rootScope', 'mediaQuery', function($rootScope, mobileQuery){
     mobileQuery.watchMobile($rootScope, 'isMobile');
     $rootScope.showPlaylists = 1;
     
+    console.log(isNativeApp);
 }]);
 
 /**
@@ -296,6 +297,117 @@ stApp.remote.factory('mediaQuery', function(){
     };
 });
 
+stApp.remote.factory('webCasting', function(){
+    var applicationID = 'fbecad4f-6140-4245-8a21-7f9e3894ffa4';
+    var playlists;
+    var castApi;
+    var currentActivityId = null;
+    var currentReceiver;
+    var receivers = [];
+
+    //control
+    var control = {};
+    control.playlists;
+    control.setEvents = function(){
+        var _this = this,
+            $doc = $(document);
+
+        $doc.on('click', 'a#stop_casting', function() {
+            castApi.stopActivity(currentActivityId, function(){
+                if(currentActivityId){
+                    currentActivityId = null;
+                }
+            });
+        });
+
+    };
+
+    control.startPlaylist = function(playlist) {
+        castApi.sendMessage(currentActivityId, 'channelcast', {
+            type: 'start_playlist',
+            data: {
+                playlist: playlist
+            }
+        });
+    };
+
+    control.playMedia = function() {
+        castApi.sendMessage(currentActivityId, 'channelcast', {type: 'play'});
+    };
+
+    control.pauseMedia = function() {
+        castApi.sendMessage(currentActivityId, 'channelcast', {type: 'pause'});
+    };
+
+    control.onMessage = function(event) {
+        console.log(event);
+    };
+
+    control.initializeCastApi = function(){
+        castApi = new cast.Api();
+        castApi.addReceiverListener(applicationID, control.onReceiverList);
+    };
+    
+    control.onReceiverList = function(list) {
+        if( list.length > 0 ) {
+            console.log("receiver list" + list);
+            var receiverDiv = document.getElementById('receivers');
+            var temp = ''; 
+
+            for( var i=0; i < list.length; i++ ) {
+                receivers.push(list[i]);
+                temp += '<li><a href="#" id="cast' + list[i].id + '" onclick="control.castMedia(' + i + ')">' + list[i].name + '</a></li>';
+            }
+            console.log(temp);
+            receiverDiv.innerHTML = temp;
+        } else {
+            console.log("receiver list empty");
+            //document.getElementById("receiver_msg").innerHTML = "No Chromecast devices found";
+        }
+    }
+    
+    control.castMedia = function(i) {
+        console.log("casting media to" + receivers[i]);
+        var _this = this;
+        currentReceiver = receivers[i];
+
+        var launchRequest = new cast.LaunchRequest(applicationID, receivers[i]);
+        launchRequest.parameters = '';
+
+        //var loadRequest = new cast.MediaLoadRequest(currentMedia);
+        //loadRequest.autoplay = true;
+
+        castApi.launch(launchRequest, function(status) {
+            if (status.status == 'running') {
+                currentActivityId = status.activityId;
+                castApi.sendMessage(currentActivityId, 'channelcast', {type: 'launched'});
+                castApi.addMessageListener(currentActivityId, 'channelcast', control.onMessage.bind(_this));
+            } else {
+                console.log('Launch failed: ' + status.errorString);
+            }
+        });
+    }
+
+    control.setEvents();
+    control.getPlaylists();
+
+    if (window.cast && window.cast.isAvailable) {
+        // Already initialized
+        control.initializeCastApi();
+    } else {
+        // Wait for API to post a message to us
+        window.addEventListener("message", function(event) {
+            if (event.source == window 
+                && event.data 
+                && event.data.source == "CastApi" 
+                && event.data.event == "Hello"){
+                control.initializeCastApi();
+            }
+        });
+    };
+
+});
+
 /**
  * filters
  */
@@ -305,140 +417,7 @@ stApp.remote.filter('somefilt', function(){
     };
 });
 
-var applicationID = 'fbecad4f-6140-4245-8a21-7f9e3894ffa4';
-var playlists;
-var castApi;
-var currentActivityId = null;
-var currentReceiver;
-var receivers = [];
-
-//control
-var control = {};
-control.playlists;
-control.setEvents = function(){
-    var _this = this,
-        $doc = $(document);
-
-    $doc.on('click', 'a#stop_casting', function() {
-        castApi.stopActivity(currentActivityId, function(){
-            if(currentActivityId){
-                currentActivityId = null;
-            }
-        });
-    });
-
-};
-
-control.getPlaylists = function(){
-
-    return;
-    $.ajax({
-        type: 'GET',
-        url: 'playlists.json',
-        dataType: 'json',
-        success: function(data){
-            control.playlists = data.playlists;
-            var $list = $('ul#playlists');
-            data.playlists.forEach(function(playlist, i){
-                $list.append('<li data-id="' + i + '">' + 
-                    '<h4>' + playlist.name + '</h4>' +
-                    '<p>' + playlist.description + '</p>' +
-                    '</li>');
-            });
-        },
-        error: function(XMLHttpRequest, text, err){
-            console.log(text);
-            console.log(err);
-        }
-    });
-};
-
-control.startPlaylist = function(playlist) {
-    castApi.sendMessage(currentActivityId, 'channelcast', {
-        type: 'start_playlist',
-        data: {
-            playlist: playlist
-        }
-    });
-};
-
-control.playMedia = function() {
-    castApi.sendMessage(currentActivityId, 'channelcast', {type: 'play'});
-};
-
-control.pauseMedia = function() {
-    castApi.sendMessage(currentActivityId, 'channelcast', {type: 'pause'});
-};
-
-control.onMessage = function(event) {
-    console.log(event);
-};
-
-control.setEvents();
-control.getPlaylists();
-
-if (window.cast && window.cast.isAvailable) {
-    // Already initialized
-    initializeCastApi();
-} else {
-    // Wait for API to post a message to us
-    window.addEventListener("message", function(event) {
-        if (event.source == window 
-            && event.data 
-            && event.data.source == "CastApi" 
-            && event.data.event == "Hello"){
-            initializeCastApi();
-        }
-    });
-};
-
-initializeCastApi = function() {
-    castApi = new cast.Api();
-    castApi.addReceiverListener(applicationID, onReceiverList);
-};
-
-function onReceiverList(list) {
-    if( list.length > 0 ) {
-        console.log("receiver list" + list);
-        var receiverDiv = document.getElementById('receivers');
-        var temp = ''; 
-
-        for( var i=0; i < list.length; i++ ) {
-            receivers.push(list[i]);
-            temp += '<li><a href="#" id="cast' + list[i].id + '" onclick="castMedia(' + i + ')">' + list[i].name + '</a></li>';
-        }
-        console.log(temp);
-        receiverDiv.innerHTML = temp;
-    } else {
-        console.log("receiver list empty");
-        //document.getElementById("receiver_msg").innerHTML = "No Chromecast devices found";
-    }
-}
-
-
-
-function castMedia(i) {
-    console.log("casting media to" + receivers[i]);
-    var _this = this;
-    currentReceiver = receivers[i];
-
-    var launchRequest = new cast.LaunchRequest(applicationID, receivers[i]);
-    launchRequest.parameters = '';
-
-    //var loadRequest = new cast.MediaLoadRequest(currentMedia);
-    //loadRequest.autoplay = true;
-
-    castApi.launch(launchRequest, function(status) {
-        if (status.status == 'running') {
-            currentActivityId = status.activityId;
-            castApi.sendMessage(currentActivityId, 'channelcast', {type: 'launched'});
-            castApi.addMessageListener(currentActivityId, 'channelcast', control.onMessage.bind(_this));
-        } else {
-            console.log('Launch failed: ' + status.errorString);
-        }
-    });
-}
-
+/*
 function launchCallback(status) {
   if( status.success == true ) {
     var icon_id = currentReceiver.id;
@@ -467,3 +446,4 @@ function setVolume(v) {
       new cast.MediaVolumeRequest(v, false),
       function() {});
 }
+*/
